15bec721ec3e1c36c5fa6237500e94e1fa88148d
mtfum <f.yamanaka1127@gmail.com>
2019-12-24 18:52:54 +0900

    update test

--- Tests/CustomAlbumTests/CustomAlbumTests.swift
+++ Tests/CustomAlbumTests/CustomAlbumTests.swift
@@ -2,14 +2,13 @@ import XCTest
 @testable import CustomAlbum
 
 final class CustomAlbumTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(CustomAlbum().text, "Hello, World!")
-    }
+  func testExample() {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct
+    // results.
+  }
 
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+  static var allTests = [
+    ("testExample", testExample),
+  ]
 }
